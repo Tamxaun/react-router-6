a4df39b6a43b23bb75811c5725838bfb63223675
Dmitry <deus.main@gmail.com>
2023-08-25 15:07:30 +0300

    🛠️refactor(App) refactor routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import './App.css';
 
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { Layout } from './components/index';
 import { About, Home, Van, Vans } from './pages/index';
 
 export default function App() {
    return (
       <BrowserRouter>
-         <header>
-            <Link className="site-logo" to="/">
-               #VanLife
-            </Link>
-            <nav>
-               <Link to="/about">About</Link>
-               <Link to="/vans">Vans</Link>
-            </nav>
-         </header>
          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<Vans />} />
-            <Route path="/vans/:id" element={<Van />} />
+            <Route path="/" element={<Layout />}>
+               <Route path="/" element={<Home />} />
+               <Route path="/about" element={<About />} />
+               <Route path="/vans" element={<Vans />} />
+               <Route path="/vans/:id" element={<Van />} />
+            </Route>
          </Routes>
       </BrowserRouter>
    );

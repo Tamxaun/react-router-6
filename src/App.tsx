1fdf4e54ea2bc65c421795531eb118a063320582
Dmitry <deus.main@gmail.com>
2023-08-25 19:28:40 +0300

    🚀feat(components) add new components to App.tsx 🛠️refactor(routes) update route structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Layout } from './components/index';
+import { Dashboard, HostLayout, Income, Layout, Reviews } from './components/index';
 import { About, Home, Van, Vans } from './pages/index';
 
 export default function App() {
@@ -10,10 +10,17 @@ export default function App() {
       <BrowserRouter>
          <Routes>
             <Route path="/" element={<Layout />}>
-               <Route path="/" element={<Home />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/vans" element={<Vans />} />
-               <Route path="/vans/:id" element={<Van />} />
+               <Route index element={<Home />} />
+               <Route path="about" element={<About />} />
+               <Route path="vans">
+                  <Route index element={<Vans />} />
+                  <Route path=":id" element={<Van />} />
+               </Route>
+               <Route path="host" element={<HostLayout />}>
+                  <Route index element={<Dashboard />} />
+                  <Route path="income" element={<Income />} />
+                  <Route path="reviews" element={<Reviews />} />
+               </Route>
             </Route>
          </Routes>
       </BrowserRouter>

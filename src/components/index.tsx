0ea11c7c5b7fe20cfbbe0e514fc8b669ac1f7d5d
Dmitry <deus.main@gmail.com>
2023-08-30 19:25:23 +0300

    ⚙️chore(components) remove unused components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,4 @@
-export * from './Dashboard';
 export * from './Footer';
 export * from './Header';
 export * from './HostLayout';
-export * from './Income';
 export * from './Layout';
-export * from './Reviews';

bfe3493db856739ca6408284af315ce8356cdbf6
Dmitry <deus.main@gmail.com>
2023-08-30 18:51:23 +0300

    🛠️refactor(layout) update nav links styling

--- src/components/HostLayout/HostLayout.tsx
+++ src/components/HostLayout/HostLayout.tsx
@@ -1,15 +1,43 @@
 import { FC } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 import { HostLayoutProps } from '.';
+
+const activeStyleLink = {
+   fontWeight: 'bold',
+   textDecoration: 'underline',
+   color: '#161616',
+};
 
 export const HostLayout: FC<HostLayoutProps> = () => {
    return (
       <>
          <nav className="host-nav">
-            <Link to="/host">Dashboard</Link>
-            <Link to="/host/income/">Income</Link>
-            <Link to="/host/reviews">Reviews</Link>
+            <NavLink
+               to="/host"
+               end
+               style={({ isActive }) => {
+                  return isActive ? activeStyleLink : undefined;
+               }}
+            >
+               Dashboard
+            </NavLink>
+            <NavLink
+               to="/host/income/"
+               style={({ isActive }) => {
+                  return isActive ? activeStyleLink : undefined;
+               }}
+            >
+               Income
+            </NavLink>
+            <NavLink
+               to="/host/reviews"
+               style={({ isActive }) => {
+                  return isActive ? activeStyleLink : undefined;
+               }}
+            >
+               Reviews
+            </NavLink>
          </nav>
          <Outlet />
       </>

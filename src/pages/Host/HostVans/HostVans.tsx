824f6f020c0f71365b71a5a30dee8a2a9a1df2fd
Dmitry <deus.main@gmail.com>
2023-08-31 16:26:00 +0300

    🚀feat(HostVans) update HostVans component to fetch and display list of host vans with links to individual van pages

--- src/pages/Host/HostVans/HostVans.tsx
+++ src/pages/Host/HostVans/HostVans.tsx
@@ -1,7 +1,36 @@
-import { FC } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+import { VansType } from '../../../miragejs/index';
 import { HostVansProps } from '.';
 
-export const HostVans: FC<HostVansProps> = () => {
-   return <h1>Host Vans</h1>;
+export const HostVans: React.FC<HostVansProps> = () => {
+   const [vansData, setVansData] = React.useState<Partial<VansType[]> | null>(null);
+
+   React.useEffect(() => {
+      fetch('http://localhost:5173/api/host/vans')
+         .then((result) => result.json())
+         .then((data) => setVansData(data.vans));
+   }, []);
+
+   const hostVansEls = vansData?.map((van) => (
+      <Link to={`/host/vans/${van?.id}`} key={van?.id} className="host-van-link-wrapper">
+         <div className="host-van-single" key={van?.id}>
+            <img src={van?.imageUrl} alt={`View of ${van?.name}`} />
+            <div className="host-van-info">
+               <h3>{van?.name}</h3>
+               <p>${van?.price}/day</p>
+            </div>
+         </div>
+      </Link>
+   ));
+
+   return (
+      <section>
+         <h1 className="host-vans-title">Your listed vans</h1>
+         <div className="host-vans-list">
+            {vansData ? <section>{hostVansEls}</section> : <h2>Loading...</h2>}
+         </div>
+      </section>
+   );
 };

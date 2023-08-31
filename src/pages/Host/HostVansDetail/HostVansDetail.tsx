d2c813e03447e95a1a1328b0785c2067958bee37
Dmitry <deus.main@gmail.com>
2023-08-31 16:26:19 +0300

    🚀feat(HostVansDetail) add dynamic data fetching

--- src/pages/Host/HostVansDetail/HostVansDetail.tsx
+++ src/pages/Host/HostVansDetail/HostVansDetail.tsx
@@ -1,7 +1,37 @@
-import { FC } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
+import { VansType } from '../../../miragejs';
 import { HostVansDetailProps } from '.';
 
-export const HostVansDetail: FC<HostVansDetailProps> = () => {
-   return <h1>Host Vans Detail</h1>;
+export const HostVansDetail: React.FC<HostVansDetailProps> = () => {
+   const params = useParams();
+   const [vanData, setVanData] = React.useState<Partial<VansType> | null>(null);
+
+   React.useEffect(() => {
+      fetch(`http://localhost:5173/api/host/vans/${params.id}`)
+         .then((result) => result.json())
+         .then((data) => setVanData(data.vans));
+   }, [params]);
+
+   return (
+      <section>
+         {vanData ? (
+            <div className="host-van-detail-layout-container">
+               <div className="host-van-detail">
+                  <img src={vanData?.imageUrl} alt={`View of ${vanData?.name}`} />
+                  <div className="host-van-detail-info-text">
+                     <i className={`van-type van-type-${vanData.type}`}>
+                        {vanData?.type}
+                     </i>
+                     <h3>{vanData?.name}</h3>
+                     <h4>${vanData?.price}/day</h4>
+                  </div>
+               </div>
+            </div>
+         ) : (
+            <h2>Loading...</h2>
+         )}
+      </section>
+   );
 };

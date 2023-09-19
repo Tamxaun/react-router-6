68350d15229545558607683dc690b23500f07c5f
Dmitry <deus.main@gmail.com>
2023-09-19 15:11:36 +0300

    🛠️refactor(Header) add dynamic state text in site logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigation } from 'react-router-dom';
 
 import { HeaderProps } from '.';
 
@@ -9,11 +9,20 @@ const activeStyleLink = {
    color: '#161616',
 };
 
+const stateText = {
+   fontWeight: 'normal',
+   fontSize: '14px',
+   color: '#161616',
+   opacity: '0.5',
+};
+
 export const Header: FC<HeaderProps> = () => {
+   const navigation = useNavigation();
+   const isNormalLoad = navigation.state === 'loading' && navigation.formData == null;
    return (
       <header>
          <Link className="site-logo" to="/">
-            #VanLife
+            #VanLife <code style={stateText}>({isNormalLoad ? 'Loading' : 'Ready'})</code>
          </Link>
          <nav>
             <NavLink

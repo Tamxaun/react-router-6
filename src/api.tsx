29b4b8ba347278c6fbfbc72b88e6b066f0d02741
Dmitry <deus.main@gmail.com>
2023-09-19 15:10:36 +0300

    ⚙️chore(api) update API endpoints

--- src/api.tsx
+++ src/api.tsx
@@ -5,8 +5,20 @@ export type ErrorType = {
 } | null;
 
 export async function getVans() {
-   const res = await fetch('api/vans');
-   console.log('res', res);
+   const res = await fetch('/api/vans');
+   if (!res.ok) {
+      throw {
+         message: 'Failed to fetch vans',
+         statusText: res.statusText,
+         status: res.status,
+      };
+   }
+
+   const data = await res.json();
+   return data.vans;
+}
+export async function getVanDetail(id: string) {
+   const res = await fetch(`/api/vans/${id}`);
    if (!res.ok) {
       throw {
          message: 'Failed to fetch vans',

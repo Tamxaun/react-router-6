4d82f78de41f5798b0aaed9031d3add30a50ca80
Dmitry <deus.main@gmail.com>
2023-09-19 16:48:15 +0300

    🚀feat(api) add getHostVanDetail function

--- src/api.tsx
+++ src/api.tsx
@@ -44,3 +44,17 @@ export async function getHostVans() {
    const data = await res.json();
    return data.vans;
 }
+
+export async function getHostVanDetail(id: string) {
+   const res = await fetch(`/api/host/vans/${id}`);
+   if (!res.ok) {
+      throw {
+         message: 'Failed to fetch vans',
+         statusText: res.statusText,
+         status: res.status,
+      };
+   }
+
+   const data = await res.json();
+   return data.vans;
+}

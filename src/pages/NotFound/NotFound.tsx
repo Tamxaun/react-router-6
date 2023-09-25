114969a1128dd75a5a6ff363a309b6481de60e9c
Dmitry <deus.main@gmail.com>
2023-09-25 17:15:36 +0300

    🛠️refactor(NotFound): update import statement

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { NotFoundProps } from '.';
 
-export const NotFound: FC<NotFoundProps> = () => {
+export const NotFound: React.FC<NotFoundProps> = () => {
    return (
       <div className="not-found-container">
          <h1>Sorry, the page you were looking for was not found.</h1>

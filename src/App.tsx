6cc5cc6bb694499887df16bc0bcb349a79993b9c
Dmitry <deus.main@gmail.com>
2023-09-25 21:00:37 +0300

    🚀feat(routes) add login route ⚙️chore(routes) update routes for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-router-dom';
 
 import {
+   AuthRequired,
    Error,
    HostLayout,
    HostVanInfo,
@@ -22,6 +23,7 @@ import {
    HostVansDetail,
    hostVansLoader,
    Income,
+   Login,
    NotFound,
    Reviews,
    VanDetail,
@@ -41,20 +43,23 @@ const router = createBrowserRouter(
             element={<VanDetail />}
             loader={({ params }) => vanDetaileLoader(params.id as string)}
          />
-         <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
-            <Route
-               path="vans/:id"
-               element={<HostVansDetail />}
-               loader={({ params }) => hostVanDetaileLoader(params.id as string)}
-            >
-               <Route index element={<HostVanInfo />} />
-               <Route path="pricing" element={<HostVanPricing />} />
-               <Route path="photos" element={<HostVanPhotos />} />
+         <Route path="login" element={<Login />} />
+         <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+               <Route index element={<Dashboard />} />
+               <Route path="income" element={<Income />} />
+               <Route path="vans" element={<HostVans />} loader={hostVansLoader} />
+               <Route
+                  path="vans/:id"
+                  element={<HostVansDetail />}
+                  loader={({ params }) => hostVanDetaileLoader(params.id as string)}
+               >
+                  <Route index element={<HostVanInfo />} />
+                  <Route path="pricing" element={<HostVanPricing />} />
+                  <Route path="photos" element={<HostVanPhotos />} />
+               </Route>
+               <Route path="reviews" element={<Reviews />} />
             </Route>
-            <Route path="reviews" element={<Reviews />} />
          </Route>
          <Route path="*" element={<NotFound />} />
       </Route>,

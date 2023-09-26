ae9c30a8ccde111f316bd819e08faa7207ad8607
Dmitry <deus.main@gmail.com>
2023-09-26 12:25:46 +0300

    🚀feat(Login) add dynamic error message rendering

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { LoginProps } from '.';
 
 export const Login: React.FC<LoginProps> = () => {
    const [loginFormData, setLoginFormData] = React.useState({ email: '', password: '' });
+   const location = useLocation();
 
    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
       e.preventDefault();
@@ -19,6 +21,9 @@ export const Login: React.FC<LoginProps> = () => {
    }
    return (
       <div className="login-container">
+         {location.state?.message && (
+            <h3 className="login-first">{location.state.message}</h3>
+         )}
          <h1>Sign in to your account</h1>
          <form onSubmit={handleSubmit} className="login-form">
             <input

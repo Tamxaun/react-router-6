f340781139404936955efa0389fe953d3419bddc
Dmitry <deus.main@gmail.com>
2023-09-26 16:41:25 +0300

    🛠️refactor(Login) refactor form submission logic 🚀feat(Login) add error handling for login ⚙️chore(Login) add navigate dependency

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,43 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
+import { loginUser } from '../../api';
 import { LoginProps } from '.';
+
+type ErrorType = {
+   message: string;
+   statusText: string;
+   status: string;
+} | null;
 
 export const Login: React.FC<LoginProps> = () => {
    const [loginFormData, setLoginFormData] = React.useState({ email: '', password: '' });
+   const [status, setStatus] = React.useState<string>('indle');
+   const [error, setError] = React.useState<ErrorType>(null);
+
+   const navigate = useNavigate();
    const location = useLocation();
 
-   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+   const from: string = location.state?.from || '/host';
+
+   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
       e.preventDefault();
-      console.log(loginFormData);
+
+      setStatus('submitting');
+      setError(null);
+
+      try {
+         await loginUser(loginFormData);
+         localStorage.setItem('isLoggedIn', 'true');
+         navigate(from, {
+            state: { authenticated: true, message: 'You have successfully logged in' },
+            replace: true,
+         });
+      } catch (err) {
+         setError(err as ErrorType);
+      }
+
+      setStatus('indle');
    }
 
    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -19,12 +47,14 @@ export const Login: React.FC<LoginProps> = () => {
          [name]: value,
       }));
    }
+
    return (
       <div className="login-container">
          {location.state?.message && (
-            <h3 className="login-first">{location.state.message}</h3>
+            <h3 className="login-error">{location.state.message}</h3>
          )}
          <h1>Sign in to your account</h1>
+         {error && <h3 className="login-error">{error.message}</h3>}
          <form onSubmit={handleSubmit} className="login-form">
             <input
                name="email"
@@ -40,7 +70,9 @@ export const Login: React.FC<LoginProps> = () => {
                placeholder="Password"
                value={loginFormData.password}
             />
-            <button>Log in</button>
+            <button type="submit" disabled={status === 'submitting'}>
+               {status === 'submitting' ? 'Submitting...' : 'Log in'}
+            </button>
          </form>
       </div>
    );

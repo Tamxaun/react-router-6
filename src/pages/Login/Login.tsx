1b69b8bc27b629b20cfa01be9151ef7cc1451989
Dmitry <deus.main@gmail.com>
2023-10-31 18:53:50 +0300

    🛠️refactor(Login): refactor code structure and imports to use Action

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,79 +1,60 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {
+   ActionFunctionArgs,
+   Form,
+   redirect,
+   useActionData,
+   useLocation,
+   useNavigation,
+} from 'react-router-dom';
 
 import { loginUser } from '../../api';
 import { LoginProps } from '.';
 
-type ErrorType = {
-   message: string;
-   statusText: string;
-   status: string;
-} | null;
+export async function action({ request }: ActionFunctionArgs) {
+   const formData = await request.formData();
+   const email = formData.get('email') as string;
+   const password = formData.get('password') as string;
+
+   try {
+      await loginUser({ email, password });
+
+      const url = new URL(request.url);
+      const from = url.searchParams.get('from') || '/host';
+
+      localStorage.setItem('isLoggedIn', 'true');
+
+      const response = redirect(from);
+      // @ts-expect-error // It's silly, but it works (more about: https://github.com/scrimba/learn-react-router-6#april-21-2023)
+      response.body = true;
+      return response;
+   } catch (error) {
+      console.error('An error occurred:', error);
+      return error;
+   }
+}
 
 export const Login: React.FC<LoginProps> = () => {
-   const [loginFormData, setLoginFormData] = React.useState({ email: '', password: '' });
-   const [status, setStatus] = React.useState<string>('indle');
-   const [error, setError] = React.useState<ErrorType>(null);
-
-   const navigate = useNavigate();
+   const error = useActionData() as TypeError;
+   const navigation = useNavigation();
    const location = useLocation();
 
-   const from: string = location.state?.from || '/host';
-
-   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-      e.preventDefault();
-
-      setStatus('submitting');
-      setError(null);
-
-      try {
-         await loginUser(loginFormData);
-         localStorage.setItem('isLoggedIn', 'true');
-         navigate(from, {
-            state: { authenticated: true, message: 'You have successfully logged in' },
-            replace: true,
-         });
-      } catch (err) {
-         setError(err as ErrorType);
-      }
-
-      setStatus('indle');
-   }
-
-   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-      const { name, value } = e.target;
-      setLoginFormData((prev) => ({
-         ...prev,
-         [name]: value,
-      }));
-   }
+   const status = navigation.state === 'submitting' ? 'submitting' : 'indle';
 
    return (
       <div className="login-container">
+         <h1>Sign in to your account</h1>
          {location.state?.message && (
             <h3 className="login-error">{location.state.message}</h3>
          )}
-         <h1>Sign in to your account</h1>
          {error && <h3 className="login-error">{error.message}</h3>}
-         <form onSubmit={handleSubmit} className="login-form">
-            <input
-               name="email"
-               onChange={handleChange}
-               type="email"
-               placeholder="Email address"
-               value={loginFormData.email}
-            />
-            <input
-               name="password"
-               onChange={handleChange}
-               type="password"
-               placeholder="Password"
-               value={loginFormData.password}
-            />
+         <Form className="login-form" method="post" replace>
+            <input name="email" type="email" placeholder="Email address" />
+            <input name="password" type="password" placeholder="Password" />
             <button type="submit" disabled={status === 'submitting'}>
                {status === 'submitting' ? 'Submitting...' : 'Log in'}
             </button>
-         </form>
+         </Form>
       </div>
    );
 };

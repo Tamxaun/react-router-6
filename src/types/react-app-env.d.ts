bc5a2d404e208f0632250df24dd61128abf4a30c
Dmitry <deus.main@gmail.com>
2023-10-31 18:52:02 +0300

    🚀feat(types) add ErrorType definition

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -2,3 +2,9 @@ declare module '*.png';
 declare module '*.svg';
 declare module '*.jpeg';
 declare module '*.jpg';
+
+declare type ErrorType = {
+   message: string;
+   statusText: string;
+   status: string;
+} | null;

706bebd8c7f44c8e19c17a62003e9fbdbc85d7d7
Dmitry <deus.main@gmail.com>
2023-11-03 14:24:55 +0300

    🛠️refactor(HostVans) refactor rendering of host vans list to usedefer and await with suspense

--- src/pages/Host/HostVans/HostVans.tsx
+++ src/pages/Host/HostVans/HostVans.tsx
@@ -1,34 +1,48 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Await, defer, Link, useLoaderData } from 'react-router-dom';
 
 import { getHostVans } from '../../../api';
 import { VansType } from '../../../miragejs/index';
 import { HostVansProps } from '.';
 
 export async function hostVansLoader() {
-   const data: VansType[] = await getHostVans();
-   return data;
+   return defer({ vansData: getHostVans() });
 }
 
 export const HostVans: React.FC<HostVansProps> = () => {
-   const vansData = useLoaderData() as VansType[];
+   const dataPromise = useLoaderData();
+   const { vansData } = dataPromise as { vansData: VansType[] };
 
-   const hostVansEls = vansData?.map((van) => (
-      <Link to={van?.id || ''} key={van?.id} className="host-van-link-wrapper">
-         <div className="host-van-single" key={van?.id}>
-            <img src={van?.imageUrl} alt={`View of ${van?.name}`} />
-            <div className="host-van-info">
-               <h3>{van?.name}</h3>
-               <p>${van?.price}/day</p>
+   function renderVansElenensts(loadedVans: VansType[]) {
+      const displayedVanElements = loadedVans?.map((van) => (
+         <Link to={van?.id || ''} key={van?.id} className="host-van-link-wrapper">
+            <div className="host-van-single" key={van?.id}>
+               <img src={van?.imageUrl} alt={`View of ${van?.name}`} />
+               <div className="host-van-info">
+                  <h3>{van?.name}</h3>
+                  <p>${van?.price}/day</p>
+               </div>
             </div>
+         </Link>
+      ));
+      return (
+         <div className="host-vans-list">
+            <section>{displayedVanElements}</section>
          </div>
-      </Link>
-   ));
+      );
+   }
 
    return (
       <section>
          <h1 className="host-vans-title">Your listed vans</h1>
-         <div className="host-vans-list">{hostVansEls}</div>
+         <React.Suspense fallback={<h2>Loading vans...</h2>}>
+            <Await
+               resolve={vansData}
+               errorElement={<p>Error loading package location!</p>}
+            >
+               {renderVansElenensts}
+            </Await>
+         </React.Suspense>
       </section>
    );
 };
